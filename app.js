9f5b1d33c3defa4935cbb74532e337ad901155fc
SonMinWoo <mwson98@naver.com>
2020-03-28 23:01:51 +0900

    4th day

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { users, user_detail, edit_profile, change_password } from "../controllers/userController";
+import { user_detail, edit_profile, change_password } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.user_detail, user_detail);
 userRouter.get(routes.edit_profile, edit_profile);
 userRouter.get(routes.change_password, change_password);
