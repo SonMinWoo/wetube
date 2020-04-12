fa18353a087062bd7175a49099ccd2fa6971b1e4
SonMinWoo <mwson98@naver.com>
2020-04-12 23:11:28 +0900

    13th day

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
 import globalRouter from "./router/globalRouter";
-import routes from "./routes";
+
+import "./passport";
+
 const app = express();
+
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -18,6 +23,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 
