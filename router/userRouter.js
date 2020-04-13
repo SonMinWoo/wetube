3e43bf1bfcb8b48341f5c9ce85352ce4db859fac
SonMinWoo <mwson98@naver.com>
2020-04-13 23:45:36 +0900

    14th day

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import { user_detail, edit_profile, change_password } from "../controllers/userController";
+import { onlyPrivate } from "../middlewares"
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, edit_profile);
-userRouter.get(routes.changePassword, change_password);
+userRouter.get(routes.editProfile, onlyPrivate, edit_profile);
+userRouter.get(routes.changePassword, onlyPrivate, change_password);
 userRouter.get(routes.userDetail(), user_detail);
 
 export default userRouter;
--- router/videoRouter.js
+++ router/videoRouter.js
@@ -1,22 +1,22 @@
 import express from "express";
 import routes from "../routes";
 import { getUpload, postUpload, video_detail, getEdit_video, postEditVideo , delete_video } from "../controllers/videoController";
-import { uploadVideo } from "../middlewares";
+import { uploadVideo, onlyPrivate } from "../middlewares";
 
 const videoRouter = express.Router();
 
 //Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 //Video Detail
 videoRouter.get(routes.videoDetail(), video_detail);
 
 //Edit Video
-videoRouter.get(routes.editVideo(), getEdit_video);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEdit_video);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 //Delete Video
-videoRouter.get(routes.deleteVideo(), delete_video);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, delete_video);
 
 export default videoRouter;
